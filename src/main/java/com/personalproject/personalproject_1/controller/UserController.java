94fbc8df96cd9c56b37e3bb630a261eec2a3df71
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-11-20 18:59:37 +0900

    feat : signup valid 구현

--- src/main/java/com/personalproject/personalproject_1/controller/UserController.java
+++ src/main/java/com/personalproject/personalproject_1/controller/UserController.java
@@ -3,21 +3,33 @@ package com.personalproject.personalproject_1.controller;
 import com.personalproject.personalproject_1.dto.SignupRequestDto;
 import com.personalproject.personalproject_1.dto.SignupResponseDto;
 import com.personalproject.personalproject_1.service.UserService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
+@Slf4j
 @RequestMapping("/api/user")
 @RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
     @PostMapping("/signup")
-    public ResponseEntity<SignupResponseDto> signup(@RequestBody SignupRequestDto requestDto) {
+    public ResponseEntity<SignupResponseDto> signup(@RequestBody @Valid SignupRequestDto requestDto, BindingResult bindingResult) {
         SignupResponseDto signupResponseDto = userService.signup(requestDto);
+        List<FieldError> fieldErrors = bindingResult.getFieldErrors();
+        if(fieldErrors.size() > 0) {
+            for (FieldError fieldError : bindingResult.getFieldErrors()) {
+                log.error(fieldError.getField() + " 필드 : " + fieldError.getDefaultMessage());
+            }
+        }
         return ResponseEntity.status(HttpStatus.CREATED).body(signupResponseDto);
     }
-
 }
--- src/main/java/com/personalproject/personalproject_1/entity/User.java
+++ src/main/java/com/personalproject/personalproject_1/entity/User.java
@@ -1,6 +1,8 @@
 package com.personalproject.personalproject_1.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -15,9 +17,13 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Size(min = 4, max = 10, message = "최소 4자 이상, 10자 이하의 숫자를 입력하세요")
+    @Pattern(regexp = "^[a-z0-9]*$", message = "알파벳 소문자(a~z), 숫자(0~9)만 입력 가능합니다.")
     @Column(name = "username", nullable = false, unique = true)
     private String username;
 
+    @Size(min = 8, max = 15, message = "최소 8자 이상, 15자 이하의 숫자를 입력하세요")
+    @Pattern(regexp = "^[a-zA-Z0-9]*$", message = "알파벳 대소문자(a~z, A~Z), 숫자(0~9)만 입력 가능합니다.")
     @Column(name = "password", nullable = false)
     private String password;
 
