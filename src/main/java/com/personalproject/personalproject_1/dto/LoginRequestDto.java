13eb9e16b92a204e960662e13d307bca9406d6c2
Kim-Jong-Gyu <whdrb2643@gmail.com>
2023-12-04 03:57:47 +0900

    test : UserController Signup 테스트 구현

--- src/main/java/com/personalproject/personalproject_1/dto/LoginRequestDto.java
+++ src/main/java/com/personalproject/personalproject_1/dto/LoginRequestDto.java
@@ -8,4 +8,9 @@ import lombok.Setter;
 public class LoginRequestDto {
     private String username;
     private String password;
+
+    public LoginRequestDto(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
 }
